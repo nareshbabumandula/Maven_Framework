3d8592e58f60bca516ceb5fdb7f7cd196f583832
Mandula Naresh Babu <naresh2223@gmail.com>
2016-11-21 20:30:27 -0500

    Incresed the wait time

--- src/test/java/scripts/TC01.java
+++ src/test/java/scripts/TC01.java
@@ -10,10 +10,12 @@ public class TC01 extends ActionDriver{
   public void testTC01() throws InterruptedException {
 	 logger = report.startTest("TC01");
 	 launchApplication("http://www.rightstart.com/"); 
+
+
 	 type(HomePage.txtSearch, "toys","Search Box");
 	 click(HomePage.btnSearch,"Search Button");
 	
-	 Thread.sleep(5000);
+	 Thread.sleep(6000);
 	 report.endTest(logger);
 	 report.flush();
 	  
